79f998b312ae8a0af8df7867299ffe08c178cd22
wja8 <wja8@sfu.ca>
2016-10-04 17:08:30 -0700

    Create functions in NPC class

--- include/Npc.h
+++
@@ -1,9 +0,0 @@
-class Npc {
-  int armor, exp, gold, id, level, thac0;
-  string damage, hit, shortdesc;
-  std::vector<string> description, keywords, longdesc;
-public:
-  Npc (int,string);
-  int getId () {return (id);}
-
-};
