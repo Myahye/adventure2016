ca13cfdd72d0fe65f3f4c30dad2f1592b3ef51ad
Kieran <octavian13@shaw.ca>
2016-10-07 12:20:24 -0700

    Added doors to rooms, moved doors and rooms to single lib file

---
+++ b/lib/baseClasses/CMakeFiles/baseClass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baseClass.dir/room.cpp.o"
+  "CMakeFiles/baseClass.dir/door.cpp.o"
+  "../libbaseClass.pdb"
+  "../libbaseClass.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/baseClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ b/lib/baseClasses/CMakeFiles/baseClasses.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baseClasses.dir/room.cpp.o"
+  "CMakeFiles/baseClasses.dir/door.cpp.o"
+  "../libbaseClasses.pdb"
+  "../libbaseClasses.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/baseClasses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
