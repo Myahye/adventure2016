6daadb0a771cd43e1150800bcdf0433e7e9d239d
Kieran <octavian13@shaw.ca>
2016-10-14 13:11:07 -0700

    Added template model classes to start implementing walk features in

---
+++ single-threaded-networking/lib/model/Model.cpp
@@ -0,0 +1,28 @@
+#ifndef model_h
+#define model_h
+
+
+class Model {
+
+private:
+
+
+
+public:
+
+  //CommandParse(pathtoyaml);
+  Model();
+  //players
+  //rooms
+  //npcs
+  //objects
+
+  //Model::yamlParse
+
+  //Methods for players to interact with room
+  //Model::movePlayer(PlayerId, currentRoomID, Direction)
+  //Model::get
+  //Model::
+
+};
+#endif /* commandparse_h */
