afe1c3c73d2097719a0f7cad7e1f2967ac7a99d5
Kieran <octavian13@shaw.ca>
2016-10-14 13:14:24 -0700

    Cleaned up CommandParse.cpp, waiting for further progress on model class before continuing development

--- single-threaded-networking/lib/model/CommandParse.cpp
+++ single-threaded-networking/lib/model/CommandParse.cpp
@@ -5,39 +5,54 @@ using namespace networking;
 
 CommandParse::CommandParse() {}
 
-std::unordered_map<std::string, std::string> commands {{"Create","Create "},{"Look","Look "},{"Go","Go "},{"Read","Read "},{"Attack","Attack "},{"Say","Say "},{"ListCommands","commands"},};
+std::unordered_map<std::string, std::string> commands {{"Create","create "},{"Look","look "},{"Go","go "},{"Read","read "},{"Attack","attack "},{"Say","say "},{"ListCommands","ls "},};
 
 
 
 std::deque<Message>
 CommandParse::parseCommands(const std::deque<Message>& clientMessageQueue, std::vector<Connection>& clients) {
 
+
+
     std::deque<Message> outgoing;
 
     for (auto& message : clientMessageQueue) {
-      if (boost::istarts_with(message.text,"Create")) {
+
+      std::string messageText = message.text;
+      std::transform(messageText.begin(), messageText.end(), messageText.begin(), ::tolower);
+
+      if (boost::istarts_with(messageText,commands["Create"])) {
         std::string messageText = std::to_string(message.connection.id) + ">asdfasdfasdf Kieran " + /*handleCreateCommand(message)*/ + "\n";
         outgoing.push_back({message.connection, messageText});
-      } else if (boost::istarts_with(message.text,commands["Look"])) {
+      }
+      else if (boost::istarts_with(messageText,commands["Look"])) {
+        std::string messageText = std::to_string(message.connection.id) + "> " + /*handleCreateCommand(message)*/ + "\n";
+        outgoing.push_back({messageText, messageText});
+      }
+      else if (boost::istarts_with(messageText,commands["Go"])) {
         std::string messageText = std::to_string(message.connection.id) + "> " + /*handleCreateCommand(message)*/ + "\n";
         outgoing.push_back({message.connection, messageText});
-      } else if (boost::istarts_with(message.text,commands["Go"])) {
+      }
+      else if (boost::istarts_with(messageText,commands["Read"])) {
         std::string messageText = std::to_string(message.connection.id) + "> " + /*handleCreateCommand(message)*/ + "\n";
         outgoing.push_back({message.connection, messageText});
-      } else if (boost::istarts_with(message.text,commands["Read"])) {
+      }
+      else if (boost::istarts_with(messageText,commands["Attack"])) {
         std::string messageText = std::to_string(message.connection.id) + "> " + /*handleCreateCommand(message)*/ + "\n";
         outgoing.push_back({message.connection, messageText});
-      } else if (boost::istarts_with(message.text,commands["Attack"])) {
+      }
+      else if (boost::istarts_with(messageText,commands["Say"])) {
+        std::for_each(clients.begin(), clients.end(), [&message,&outgoing] (Connection& c)
+          { outgoing.push_back({c,std::string(std::to_string(message.connection.id) + "> " + message.text.substr(4) + "\n")}); });
+      }
+      else if (boost::iequals(messageText, commands["ListCommands"])) {
         std::string messageText = std::to_string(message.connection.id) + "> " + /*handleCreateCommand(message)*/ + "\n";
         outgoing.push_back({message.connection, messageText});
-      } else if (boost::istarts_with(message.text,commands["Say"])) {
-        std::for_each(clients.begin(), clients.end(), [&message,&outgoing] (Connection& c) { outgoing.push_back({c,std::string(std::to_string(message.connection.id) + "> " + message.text.substr(4) + "\n")}); });
-      } else if (boost::iequals(message.text, commands["ListCommands"])) {
-        std::string messageText = std::to_string(message.connection.id) + "> " + /*handleCreateCommand(message)*/ + "\n";
-        outgoing.push_back({message.connection, messageText});
-      } else {
+      }
+      else {
         //Will output all other message types sent for now for testing purposes
-        std::for_each(clients.begin(), clients.end(), [&message,&outgoing] (Connection& c) { outgoing.push_back({c,std::string(std::to_string(message.connection.id) + "> " + message.text + "\n")}); });
+        std::for_each(clients.begin(), clients.end(), [&message,&outgoing] (Connection& c)
+          { outgoing.push_back({c,std::string(std::to_string(message.connection.id) + "> " + message.text + "\n")}); });
       }
     }
     return outgoing;
