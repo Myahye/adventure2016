78b7f2d0506e9ec521979fa6f6556c09627c84ed
wja8 <wja8@sfu.ca>
2016-11-24 01:00:02 -0800

    A door leads to another room with direction

--- single-threaded-networking/lib/yamlParser/YamlParseBuild.cpp
+++ single-threaded-networking/lib/yamlParser/YamlParseBuild.cpp
@@ -65,7 +65,82 @@ void YamlParseBuild::buildNpcs(std::unordered_map<int,Npc>& buildAllNpcs){
 																  
 }
 
+std::pair<int,Player> YamlParseBuild::parsePlayers(const YAML::Node& node){
+	Player playerClass{
+		node["id"].as<int>(),
+		node["username"].as<std::string>(),
+		node["password"].as<std::string>()
+	};
+	if(node["armor"]){
+		playerClass.playerCharacter.setArmor(node["armor"].as<int>());
+	}
+	if(node["exp"]){
+		playerClass.playerCharacter.setExp(node["exp"].as<int>());
+	}
+	if(node["gold"]){
+		playerClass.playerCharacter.setGold(node["gold"].as<int>());
+	}
+	if(node["level"]){
+		playerClass.playerCharacter.setLevel(node["level"].as<int>());
+	}
+	if(node["health"]){
+		playerClass.playerCharacter.setMaxHealth(node["health"].as<int>());
+	}
+	if(node["mana"]){
+		playerClass.playerCharacter.setMaxMana(node["mana"].as<int>());
+	}
+	if(node["thac0"]){
+		playerClass.playerCharacter.setThac0(node["thac0"].as<int>());
+	}
+	if(node["damage"]){
+		std::string damage = node["damage"].as<std::string>();
+		playerClass.playerCharacter.setDamage(damage);
+	}
+	if(node["hit"]){
+		std::string hit = node["hit"].as<std::string>();
+		playerClass.playerCharacter.setHit(hit);
+	}
+	if(node["description"]){
+		std::vector<std::string> descV = setStringVectorHelper(node["description"]);
+		playerClass.playerCharacter.setDescription(descV); 
+	}
+	if(node["shortdesc"]){
+		std::string shortdesc = node["shortdesc"].as<std::string>();
+		playerClass.playerCharacter.setShortDesc(shortdesc);
+	}
+	if(node["keywords"]){
+		std::vector<std::string> keywordsV = setStringVectorHelper(node["keywords"]);
+		playerClass.playerCharacter.setKeywords(keywordsV);
+	}
+	if(node["longdesc"]){
+		std::vector<std::string> longdescV = setStringVectorHelper(node["longdesc"]);
+		playerClass.playerCharacter.setLongDesc(longdescV);
+	}
+	if(node["inventory"]){
+		std::unordered_map<int,std::vector<Object> > inventory;
+		playerClass.playerCharacter.setInventory(inventory);
+		std::vector<Object> objectsV;
+		buildObjectsForPlayerInventory(node["inventory"], objectsV);
+		for (auto& eachObject : objectsV){
+			playerClass.playerCharacter.addObjectToInventory(eachObject, 10); // number 10 needs to be changed
+		}
+	}
+	if(node["equipment"]){
+		std::unordered_map<int,Object> equipment;
+		buildObjectsForPlayerEquipment(node["equipment"], equipment);
+		playerClass.playerCharacter.setEquipment(equipment);
+	}
 
+	return std::make_pair(playerClass.playerCharacter.getId(),playerClass);
+}
+
+void YamlParseBuild::buildPlayers(std::unordered_map<int,Player>& buildAllPlayers){
+	//std::unordered_map<int,Npc> buildAllNpcs;
+	std::transform(fileNode.begin(), fileNode.end(), std::inserter( buildAllPlayers, buildAllPlayers.end() ), 
+					[this](const YAML::Node& node) { return this->parsePlayers(node); }); //if we dont use [this] error:invalid use of non static member function
+	//return buildAllNpcs;
+																  
+}
 
 /*********Parses and builds Object's from yaml file****************/
 std::pair<int,Object> YamlParseBuild::parseObjects(const YAML::Node& node){
@@ -125,13 +200,80 @@ std::pair<int,Object> YamlParseBuild::parseObjects(const YAML::Node& node){
 }
 
 void YamlParseBuild::buildObjects(std::unordered_map<int,Object>& buildAllObjects){
-	const YAML::Node&  Object_Node = fileNode["OBJECTS"];
+	const YAML::Node& Object_Node = fileNode["OBJECTS"];
 	//std::unordered_map<int,Object> buildAllObjects;
 	
 	std::transform(Object_Node.begin(),Object_Node.end(), std::inserter( buildAllObjects, buildAllObjects.end() ), 
 					[this](const YAML::Node& node) { return this->parseObjects(node); });
 	//return buildAllObjects;
 }
+void YamlParseBuild::buildObjectsForPlayerEquipment(const YAML::Node& object_node, std::unordered_map<int,Object>& inventoryObjects){
+
+	std::transform(object_node.begin(),object_node.end(), std::inserter( inventoryObjects, inventoryObjects.end() ), 
+					[this](const YAML::Node& node) { return this->parseObjects(node); });
+}
+
+Object YamlParseBuild::parseObjectsForPlayerInventory(const YAML::Node& node){
+
+	//Object objectClass{node["id"].as<int>(), node["item_type"].as<std::string>()};
+	Object objectClass;
+	objectClass.setId(node["id"].as<int>());
+
+	if(node["cost"]){
+		objectClass.setCost(node["cost"].as<int>());
+	}
+	if(node["weight"]){
+		objectClass.setWeight(node["weight"].as<int>());
+	}
+	if(node["item_type"]){
+		std::string item_type = node["item_type"].as<std::string>();
+		objectClass.setItemType(item_type);
+	}
+	if(node["shortdesc"]){
+		std::string shortdesc = node["shortdesc"].as<std::string>();
+		objectClass.setShortDesc(shortdesc);
+	}
+	if(node["attributes"]){
+		std::vector<std::string> attributesV = setStringVectorHelper(node["attributes"]);
+		objectClass.setAttributes(attributesV); 
+	}
+	if(node["keywords"]){
+		std::vector<std::string> keywordsV = setStringVectorHelper(node["keywords"]);
+		objectClass.setKeywords(keywordsV);
+	}
+	if(node["longdesc"]){
+		std::vector<std::string> longdescV = setStringVectorHelper(node["longdesc"]);
+		objectClass.setLongDesc(longdescV);
+	}
+	if(node["wear_flags"]){
+		std::vector<std::string> wearFlagsV = setStringVectorHelper(node["wear_flags"]);
+		objectClass.setWearFlags(wearFlagsV);
+	}
+	if(node["extra"]){
+		const YAML::Node& extra_node = node["extra"];
+		std::vector < std::pair< std::vector<std::string>, std::vector<std::string> > > extraV;
+		std::pair< std::vector<std::string>, std::vector<std::string> > extraP;
+		std::vector<std::string> extraDescV;
+		std::vector<std::string> extraKeywordsV;	
+		
+		for (auto &j : extra_node){
+			extraDescV = setStringVectorHelper(j["desc"]);
+			extraKeywordsV = setStringVectorHelper(j["keywords"]);
+			extraP = std::make_pair(extraDescV, extraKeywordsV);
+			extraV.push_back(extraP);
+		}
+		//std::cout << "size: " << extraDescV.size() << std::endl;
+		//std::cout << "size: " << extraKeywordsV.size() << std::endl;
+		objectClass.setExtra(extraV);
+	}
+	return objectClass;
+}
+
+void YamlParseBuild::buildObjectsForPlayerInventory(const YAML::Node& inventory_node, std::vector<Object>& objectsV){
+	std::transform(inventory_node.begin(),inventory_node.end(), std::inserter( objectsV, objectsV.end() ), 
+					[this](const YAML::Node& node) { return this->parseObjectsForPlayerInventory(node); });
+}
+
 //maybe change to take in npc,object,room maps as parameters and pass in pointers to rooms/npc/objects to the constructors of resetNPC etc.
 //change to use unique_ptr and move ownership to vector
 /*
@@ -266,21 +408,20 @@ void YamlParseBuild::buildResets(std::vector< std::unique_ptr< Reset > >&  reset
 
 Door YamlParseBuild::parseDoors(const YAML::Node& node){
 	Door doorObject;
-
+	if (node["dir"]){
+		std::string direction = node["dir"].as<std::string>();
+		doorObject.setDir(direction);
+	}
 	if(node["desc"]){
 		std::vector<std::string> descV = setStringVectorHelper(node["desc"]);
 		doorObject.setDesc(descV);
-	}
-	if(node["dir"]){
-		std::string direction = node["dir"].as<std::string>();
-		doorObject.setDir(direction);
 	}
 	if(node["keywords"]){
 		std::vector<std::string> keywordsV = setStringVectorHelper(node["keywords"]);
 		doorObject.setDesc(keywordsV);
 	}
 	if(node["to"]){
-		doorObject.setDoorId(node["to"].as<int>());
+		doorObject.setDestinationId(node["to"].as<int>());
 	}
 	return doorObject;
 }
@@ -295,7 +436,10 @@ void YamlParseBuild::buildDoors(const YAML::Node& door_node, std::vector<Door>& 
 
 std::pair<int,Room> YamlParseBuild::parseRooms(const YAML::Node& node){
 	Room roomObject;
-
+	int roomId = node["id"].as<int>();
+	if(node["id"]){
+		roomObject.setRoomId(node["id"].as<int>());
+	}
 	if(node["desc"]){
 		std::vector<std::string> descV = setStringVectorHelper(node["desc"]);
 		roomObject.setDescription(descV);
@@ -303,7 +447,10 @@ std::pair<int,Room> YamlParseBuild::parseRooms(const YAML::Node& node){
 	if(node["doors"]){
 		std::vector<Door> doorsV;
 		buildDoors(node["doors"], doorsV);
-		roomObject.addVectorDoors(doorsV);
+		for (auto& eachDoor : doorsV){
+			eachDoor.setDoorId(roomId);
+			roomObject.addDoor(eachDoor);
+		}
 	}
 	if(node["extended_descriptions"]){
 		const YAML::Node& extended_desc_node = node["extended_descriptions"];
@@ -311,7 +458,6 @@ std::pair<int,Room> YamlParseBuild::parseRooms(const YAML::Node& node){
 		std::pair< std::vector<std::string>, std::vector<std::string> > extendedDescP;
 		std::vector<std::string> descV;
 		std::vector<std::string> keywordsV;	
-		
 		for (auto &j : extended_desc_node){
 			descV = setStringVectorHelper(j["desc"]);
 			keywordsV = setStringVectorHelper(j["keywords"]);
@@ -321,9 +467,6 @@ std::pair<int,Room> YamlParseBuild::parseRooms(const YAML::Node& node){
 		//std::cout << "size: " << extraDescV.size() << std::endl;
 		//std::cout << "size: " << extraKeywordsV.size() << std::endl;
 		roomObject.setExtendedDesc(extendedDescV);
-	}
-	if(node["id"]){
-		roomObject.setRoomId(node["id"].as<int>());
 	}
 	if(node["name"]){
 		std::string roomName = node["name"].as<std::string>();
@@ -390,7 +533,7 @@ Spells YamlParseBuild::parseSpells(const YAML::Node& node){
 void YamlParseBuild::buildSpells(std::vector<Spells>& spellsV){
 	const YAML::Node& defense_node = fileNode["defense"];
 	//std::unordered_map<int,Object> buildAllObjects;
-	
+
 	std::transform(defense_node.begin(),defense_node.end(), std::inserter(spellsV, spellsV.end()), 
 					[this](const YAML::Node& node) { return this->parseSpells(node); });
 }
