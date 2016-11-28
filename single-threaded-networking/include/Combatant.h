e43e970b00e494ae7da92104be1b3d9f1f6f8ae7
gsgill <gsgill@sfu.ca>
2016-11-27 16:43:42 -0800

    pair programming with keiran combatant class

--- single-threaded-networking/include/Combatant.h
+++ single-threaded-networking/include/Combatant.h
@@ -1,17 +1,22 @@
 #ifndef COMBATANT_H
 #define COMBATANT_H
 
+#include "Server.h"
+#include "Character.h"
+#include "Players.h"
+
 class Combatant {
 private:
-  bool isNpc;
+  //bool isNpc;
   networking::Connection connection;
-
+  Character character;
 public:
-  Combatant(bool isNpc_, networking::Connection* connection_, Character* character_);
+//  Combatant(bool isNpc_, networking::Connection connection_, Character* character_);
+  Combatant(networking::Connection connection_,Character character_);
 
-  bool isNpc();
-  networking::message sendMessage(String message);
-  void attack(int amount);
+  //bool isNpc();
+  networking::Message sendMessage(const std::string& message);
+  networking::Message attack(int amount, const std::string& targetName );
 
 }
 
--- single-threaded-networking/lib/model/Combatant.cpp
+++ single-threaded-networking/lib/model/Combatant.cpp
@@ -1,5 +1,21 @@
-//isNpc
+#include"Combatant.h"
 
-//Connection
+Combatant::Combatant() {}
+Combatant::Combatant(networking::Connection connection_,Character& character_)
+: connection{connection_},character{character_}{}
 
-//Character
+networking::Message
+Combatant::attack(int ammout, const string& targetName){
+  int currHealth = character.getHealth();
+  character.setHealth(currHealth-ammount);
+  std::string response = "ALERT > You have been attacked by " + targetNamename + "\n\n";
+  networking::Message retMessage{connection,response};
+  return retMessage;
+}
+
+networking::Message
+Combatant::sendMessage(const std::string& sourceMessage){
+  std::string response = sourceMessage;
+  networking::Message retMessage(connection,sourceMessage);
+  return retMessage;
+}
